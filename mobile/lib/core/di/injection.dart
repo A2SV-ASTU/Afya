7bebbb54586cf89c52eb7a306cc4fca94f2e49c3
Bereket-Ketema <bereketketema866@gmail.com>
2026-08-24 11:37:05 +0300

    feat(mobile): setup dio client with persistent cookie session

--- mobile/lib/core/di/injection.dart
+++ mobile/lib/core/di/injection.dart
@@ -1,15 +1,28 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
+import 'package:mobile/core/network/api_client.dart';
+import 'package:mobile/core/network/auth_cookie_manager.dart';
 
 final GetIt sl = GetIt.instance;
 
 Future<void> initServiceLocator() async {
-  // -------------------------------------------------------------
-  // Core Infrastructure (Dio, CookieManager, SecureStorage, Hive)
-  // Registered in AFYA-CORE-02
-  // -------------------------------------------------------------
+  // Storage
+  const FlutterSecureStorage secureStorage = FlutterSecureStorage();
+  sl.registerLazySingleton<FlutterSecureStorage>(() => secureStorage);
 
-  // -------------------------------------------------------------
-  // Feature Slices will be registered by their respective owners
-  // sl.registerLazySingleton<AuthRepository>(() => AuthRepositoryImpl(...));
-  // -------------------------------------------------------------
+  // Auth Cookie Manager
+  final AuthCookieManager cookieManager =
+      AuthCookieManager(secureStorage: sl<FlutterSecureStorage>());
+  await cookieManager.init();
+  sl.registerLazySingleton<AuthCookieManager>(() => cookieManager);
+
+  // Dio & API Client
+  sl.registerLazySingleton<Dio>(() => Dio());
+  sl.registerLazySingleton<ApiClient>(
+    () => ApiClient(
+      dio: sl<Dio>(),
+      cookieManager: sl<AuthCookieManager>(),
+    ),
+  );
 }
