41c00615d29e99d717500bf0d8987ec17de2c8f6
yfreh <yfreheiwot@gmail.com>
2026-09-03 14:49:42 +0300

    fix(mobile): resolve android gradle conflict

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -14,14 +14,10 @@ android {
         isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-        isCoreLibraryDesugaringEnabled = true
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.mobile"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -30,8 +26,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -47,18 +41,6 @@ kotlin {
     }
 }
 
-dependencies {
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
-}
-
 flutter {
     source = "../.."
-<<<<<<< HEAD
 }
-
-dependencies {
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
-}
-=======
-}
->>>>>>> 37d2c69 (feat(mobile): implement profile and vitals features)
